00c22736923bf0d03b34d4baf529b6919fe637b7
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2025-11-01 18:09:40 +0100

    update handlers
    included type declarations and API handlers for JSON responses.

--- api.go
+++ api.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -8,19 +11,25 @@ import (
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
 	w.WriteHeader(status)
 	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
 }
 
-type apiFunc func (http.ResponseWriter, http.Request) error
+type apiFunc func (http.ResponseWriter, *http.Request) error
+
+type APIError struct {
+	Error string
+}
 
 func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
 			// handle the error
+			WriteJSON(w, http.StatusBadRequest, APIError{Error: err.Error()})
 		}
 	}
 }
 
-type APIServer struct {
+type APIServer struct { 
 	address string
 }
 
@@ -32,25 +41,42 @@ func NewAPIServer(address string) *APIServer {
 
 func (s *APIServer) Run() {
 	router := mux.NewRouter()
-	router.HandleFunc("/account", s.handleAccount())
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+	log.Println("JSON API server running on port: ", s.address)
+
+	// May need to change it to ListenAndServeTLS for added security.
+	http.ListenAndServe(s.address, router)
 }
 
-func (s *APIServer) handleAccount(w http.ResponseWriter, r http.Request) error {
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+
+	if r.Method == "DELETE" {
+		return s.handleCloseAccount(w, r)
+	}
+	return fmt.Errorf("Method not allowed %s", r.Method)
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	account := NewAccount("Daniel", "DO")
+
+	return WriteJSON(w, http.StatusOK, account)
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func (s *APIServer) handleGetAccount(w http.ResponseWriter, r http.Request) error {
+func (s *APIServer) handleCloseAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r http.Request) error {
-	return nil
-}
-
-func (s *APIServer) handleCloseAccount(w http.ResponseWriter, r http.Request) error {
-	return nil
-}
-
-func (s *APIServer) handleTransferAccount(w http.ResponseWriter, r http.Request) error {
+func (s *APIServer) handleTransferAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
